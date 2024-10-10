0d23485f4b85a63f2c7f5d32ffcedfd81831ed89
BIGGY00 <bowbigzaza2014@gmail.com>
2024-10-10 12:59:42 +0700

    Edit mail pattern

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -20,15 +20,18 @@ export class MailService {
           {
             From: {
               Email: 's6430613010@phuket.psu.ac.th',
-              Name: 'Earthquake-Estimation Website',
+              Name: 'Earthquake-Estimation Website Team',
             },
             To: [
               {
                 Email: email,
               },
             ],
-            Subject: 'Thank you for registering!',
-            TextPart: 'Thank you for following us!',
+            Subject: 'Welcome to the Earthquake-Estimation Community!',
+            TextPart:
+              'Dear valued member,\n\nThank you for registering with the Earthquake-Estimation platform. We are excited to have you on board! Stay tuned for the latest updates on seismic activities and safety information. If you have any questions, feel free to contact us at any time.\n\nBest regards,\nEarthquake-Estimation Team',
+            HTMLPart:
+              '<p>Dear valued member,</p><p>Thank you for registering with the <strong>Earthquake-Estimation</strong> platform. We are excited to have you on board! Stay tuned for the latest updates on seismic activities and safety information. If you have any questions, feel free to contact us at any time.</p><p>Best regards,<br/><strong>Earthquake-Estimation Team</strong></p>',
           },
         ],
       });
